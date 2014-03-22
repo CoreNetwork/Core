3c11951ceaea7e936a80d320c2d9bc3ee3e5be40
matejdro <matejdro@gmail.com>
2014-03-22 20:43:47 +0100

    Fixed #15

--- src/us/corenetwork/core/teleport/TeleportListener.java
+++ src/us/corenetwork/core/teleport/TeleportListener.java
@@ -1,12 +1,13 @@
 package us.corenetwork.core.teleport;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class TeleportListener implements Listener {
 	
-	@EventHandler(ignoreCancelled = true)
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
 	public void onPlayerJoin(PlayerJoinEvent event)
 	{
 		OfflineTeleportation.playerLoggedIn(event.getPlayer());
