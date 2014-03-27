cf93bfcb75451e8107a4739584746ecb2ac561b0
Jan S <ginafff@gmail.com>
2014-03-27 22:19:01 +0100

    #23 continuation,/god [silent]  /ungod [silent]

--- src/us/corenetwork/core/player/commands/UngodCommand.java
+++ src/us/corenetwork/core/player/commands/UngodCommand.java
@@ -20,24 +20,62 @@ public class UngodCommand extends BasePlayerCommand {
 	@Override
 	public void run(CommandSender sender, String[] args) 
 	{
-		if (args.length == 0 || args.length > 2)
+		if (args.length > 2)
 		{
 			PlayerUtils.Message("Usage : /ungod <player> [silent]", sender);
 			return;
 		}
 
 		
-		Player player = CorePlugin.instance.getServer().getPlayerExact(args[0]);
+		Player player = null;
 		boolean silent = false;
 		
-		if(player == null)
+		if (args.length == 0)
 		{
-			PlayerUtils.Message("Cannot find " + args[0] + " player." , sender);
-			return;
+			if (sender instanceof Player)
+			{
+				player = (Player) sender;
+			}
+			else
+			{
+				PlayerUtils.Message("You can only execute /god [silent] as player.", sender);
+				return;
+			}
 		}
-		
-		if (args.length == 2)
+		if (args.length == 1)
 		{
+			player = CorePlugin.instance.getServer().getPlayerExact(args[0]);
+			if (player == null)
+			{
+				if (args[0].toLowerCase().equals("silent"))
+				{
+					if (sender instanceof Player)
+					{
+						player = (Player) sender;
+						silent = true;
+					}
+					else
+					{
+						PlayerUtils.Message("You can only execute /ungod [silent] as player.", sender);
+						return;
+					}
+				}
+				else
+				{
+					PlayerUtils.Message("Cannot find " + args[0] + " player." , sender);
+					return;					
+				}
+			}
+		}
+		else if (args.length == 2)
+		{
+			player = CorePlugin.instance.getServer().getPlayerExact(args[0]);
+			if (player == null)
+			{
+				PlayerUtils.Message("Cannot find " + args[0] + " player." , sender);
+				return;					
+			}
+			
 			if (args[1].toLowerCase().equals("silent"))
 			{
 				silent = true;
